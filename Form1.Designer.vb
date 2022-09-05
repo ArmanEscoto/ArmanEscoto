39c602b71a52b1c9d6531ac2c8333d50c88bae09
ArmanEscoto <chrimanescoto2426@gmail.com>
2022-09-05 09:53:01 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,240 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.txtlastname = New System.Windows.Forms.Label()
+        Me.txtfirstname = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.txtmiddle = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.txtaddress = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.cmbgender = New System.Windows.Forms.Label()
+        Me.txtcontact = New System.Windows.Forms.Label()
+        Me.cmbcourse = New System.Windows.Forms.Label()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Listview1 = New System.Windows.Forms.ListView()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Modern No. 20", 8.999999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(48, 36)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(75, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Student ID."
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(129, 31)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(610, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'txtlastname
+        '
+        Me.txtlastname.AutoSize = True
+        Me.txtlastname.Font = New System.Drawing.Font("Modern No. 20", 8.999999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtlastname.Location = New System.Drawing.Point(102, 79)
+        Me.txtlastname.Name = "txtlastname"
+        Me.txtlastname.Size = New System.Drawing.Size(69, 15)
+        Me.txtlastname.TabIndex = 2
+        Me.txtlastname.Text = "Last Name"
+        '
+        'txtfirstname
+        '
+        Me.txtfirstname.AutoSize = True
+        Me.txtfirstname.Font = New System.Drawing.Font("Modern No. 20", 8.999999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtfirstname.Location = New System.Drawing.Point(337, 79)
+        Me.txtfirstname.Name = "txtfirstname"
+        Me.txtfirstname.Size = New System.Drawing.Size(72, 15)
+        Me.txtfirstname.TabIndex = 3
+        Me.txtfirstname.Text = "First Name"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(54, 107)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(215, 20)
+        Me.TextBox2.TabIndex = 4
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(293, 107)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(207, 20)
+        Me.TextBox3.TabIndex = 5
+        '
+        'txtmiddle
+        '
+        Me.txtmiddle.AutoSize = True
+        Me.txtmiddle.Font = New System.Drawing.Font("Modern No. 20", 8.249999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtmiddle.Location = New System.Drawing.Point(605, 79)
+        Me.txtmiddle.Name = "txtmiddle"
+        Me.txtmiddle.Size = New System.Drawing.Size(82, 14)
+        Me.txtmiddle.TabIndex = 6
+        Me.txtmiddle.Text = "Midlle Initial"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(524, 107)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(215, 20)
+        Me.TextBox4.TabIndex = 7
+        '
+        'txtaddress
+        '
+        Me.txtaddress.AutoSize = True
+        Me.txtaddress.Font = New System.Drawing.Font("Modern No. 20", 8.999999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtaddress.Location = New System.Drawing.Point(54, 143)
+        Me.txtaddress.Name = "txtaddress"
+        Me.txtaddress.Size = New System.Drawing.Size(52, 15)
+        Me.txtaddress.TabIndex = 8
+        Me.txtaddress.Text = "Address"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(54, 161)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(685, 20)
+        Me.TextBox5.TabIndex = 9
+        '
+        'cmbgender
+        '
+        Me.cmbgender.AutoSize = True
+        Me.cmbgender.Font = New System.Drawing.Font("Modern No. 20", 8.999999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmbgender.Location = New System.Drawing.Point(102, 197)
+        Me.cmbgender.Name = "cmbgender"
+        Me.cmbgender.Size = New System.Drawing.Size(48, 15)
+        Me.cmbgender.TabIndex = 10
+        Me.cmbgender.Text = "Gender"
+        '
+        'txtcontact
+        '
+        Me.txtcontact.AutoSize = True
+        Me.txtcontact.Font = New System.Drawing.Font("Modern No. 20", 8.999999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtcontact.Location = New System.Drawing.Point(337, 197)
+        Me.txtcontact.Name = "txtcontact"
+        Me.txtcontact.Size = New System.Drawing.Size(74, 15)
+        Me.txtcontact.TabIndex = 11
+        Me.txtcontact.Text = "Contact No."
+        '
+        'cmbcourse
+        '
+        Me.cmbcourse.AutoSize = True
+        Me.cmbcourse.Font = New System.Drawing.Font("Modern No. 20", 8.999999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmbcourse.Location = New System.Drawing.Point(605, 197)
+        Me.cmbcourse.Name = "cmbcourse"
+        Me.cmbcourse.Size = New System.Drawing.Size(45, 15)
+        Me.cmbcourse.TabIndex = 12
+        Me.cmbcourse.Text = "Course"
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(293, 226)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(207, 20)
+        Me.TextBox7.TabIndex = 14
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"BSIT", "BSTM", "BSCRIM", "BSBA"})
+        Me.ComboBox1.Location = New System.Drawing.Point(524, 225)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(215, 21)
+        Me.ComboBox1.TabIndex = 15
+        '
+        'Listview1
+        '
+        Me.Listview1.HideSelection = False
+        Me.Listview1.Location = New System.Drawing.Point(51, 262)
+        Me.Listview1.Name = "Listview1"
+        Me.Listview1.Size = New System.Drawing.Size(697, 176)
+        Me.Listview1.TabIndex = 16
+        Me.Listview1.UseCompatibleStateImageBehavior = False
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"Male", "Female"})
+        Me.ComboBox2.Location = New System.Drawing.Point(57, 226)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(212, 21)
+        Me.ComboBox2.TabIndex = 17
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.Listview1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.cmbcourse)
+        Me.Controls.Add(Me.txtcontact)
+        Me.Controls.Add(Me.cmbgender)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.txtaddress)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.txtmiddle)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.txtfirstname)
+        Me.Controls.Add(Me.txtlastname)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents txtlastname As Label
+    Friend WithEvents txtfirstname As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents txtmiddle As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents txtaddress As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents cmbgender As Label
+    Friend WithEvents txtcontact As Label
+    Friend WithEvents cmbcourse As Label
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Listview1 As ListView
+    Friend WithEvents ComboBox2 As ComboBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles txtfirstname.Click
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles txtaddress.Click
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub txtlastname_Click(sender As Object, e As EventArgs) Handles txtlastname.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        openCon()
+    End Sub
+End Class
